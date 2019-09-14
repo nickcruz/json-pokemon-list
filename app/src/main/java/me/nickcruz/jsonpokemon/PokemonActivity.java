d63db65a672c2478838e4e8abf42c50a9425f1cf
Nick Cruz <ncruz@umich.edu>
2019-09-14 16:35:49 -0700

    Set up view model to make request.

--- app/src/main/java/me/nickcruz/jsonpokemon/PokemonActivity.java
+++ app/src/main/java/me/nickcruz/jsonpokemon/PokemonActivity.java
@@ -2,11 +2,15 @@ package me.nickcruz.jsonpokemon;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class PokemonActivity extends AppCompatActivity implements PokemonAdapterListener {
 
@@ -16,16 +20,42 @@ public class PokemonActivity extends AppCompatActivity implements PokemonAdapter
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpViewModel();
+        setUpRecyclerView();
+    }
 
+    private void setUpRecyclerView() {
         RecyclerView pokemonRecyclerView = findViewById(R.id.pokemon_recycler_view);
         pokemonRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         pokemonRecyclerView.setAdapter(pokemonAdapter);
-
         pokemonAdapter.setListener(this);
+    }
+
+    private void setUpViewModel() {
+        PokemonViewModel pokemonViewModel = ViewModelProviders.of(this).get(PokemonViewModel.class);
+        pokemonViewModel.getPokemon().observe(this, new Observer<List<PokemonModel>>() {
+            @Override
+            public void onChanged(List<PokemonModel> pokemon) {
+                if (pokemon != null) {
+                    pokemonAdapter.setPokemon(pokemon);
+                }
+            }
+        });
+        pokemonViewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                showToast(errorMessage);
+            }
+        });
+        pokemonViewModel.requestPokemon();
     }
 
     @Override
     public void onPokemonSelected(@NonNull PokemonModel pokemonModel) {
-        Toast.makeText(this, String.format("Clicked on: %s", pokemonModel.getName()), Toast.LENGTH_SHORT).show();
+        showToast(String.format("Clicked on: %s", pokemonModel.getName()));
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/me/nickcruz/jsonpokemon/PokemonViewModel.java
+++ app/src/main/java/me/nickcruz/jsonpokemon/PokemonViewModel.java
@@ -1,24 +1,66 @@
 package me.nickcruz.jsonpokemon;
 
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PokemonViewModel extends ViewModel {
+public class PokemonViewModel extends AndroidViewModel {
 
-    private MutableLiveData<List<PokemonModel>> pokemon = new MutableLiveData<>((List<PokemonModel>) new ArrayList<PokemonModel>());
+    private static final String API_BASE = "https://pokeapi.co/api/v2/";
+    private static final String GET_POKEMON = "pokemon/";
 
-    public void requestPokemon() {
+    private final MutableLiveData<List<PokemonModel>> pokemon = new MutableLiveData<>((List<PokemonModel>) new ArrayList<PokemonModel>());
+    private final MutableLiveData<String> error = new MutableLiveData<>();
+    
+    private final GetPokemonResponseHandler getPokemonResponseHandler = new GetPokemonResponseHandler();
+    private final ErrorHandler errorHandler = new ErrorHandler();
+
+    public PokemonViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    void requestPokemon() {
         List<PokemonModel> value = pokemon.getValue();
         if (value == null || value.isEmpty()) {
-            // TODO(nick) GET from API
+            Volley.newRequestQueue(getApplication())
+                    .add(new StringRequest(Request.Method.GET, API_BASE + GET_POKEMON, getPokemonResponseHandler, errorHandler));
         }
     }
 
-    public LiveData<List<PokemonModel>> getPokemon() {
+    LiveData<List<PokemonModel>> getPokemon() {
         return pokemon;
+    }
+
+    LiveData<String> getError() {
+        return error;
+    }
+
+    private class GetPokemonResponseHandler implements Response.Listener<String> {
+        @Override
+        public void onResponse(String response) {
+            error.postValue(null);
+            Log.v(PokemonViewModel.class.getSimpleName(), response);
+            // TODO(nick) parse json response
+        }
+    }
+
+    private class ErrorHandler implements Response.ErrorListener {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            PokemonViewModel.this.error.postValue(error.getMessage());
+        }
     }
 }
