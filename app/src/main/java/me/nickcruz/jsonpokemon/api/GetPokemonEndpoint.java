a1bdc37149f74a1e7cd77dddd0fdf47a4bf571ad
Nick Cruz <ncruz@umich.edu>
2019-09-14 17:07:16 -0700

    Parse response in endpoint using JSONObject.

--- app/src/main/java/me/nickcruz/jsonpokemon/api/GetPokemonEndpoint.java
+++ app/src/main/java/me/nickcruz/jsonpokemon/api/GetPokemonEndpoint.java
@@ -1,5 +1,6 @@
 package me.nickcruz.jsonpokemon.api;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -19,7 +20,13 @@ public class GetPokemonEndpoint {
     public List<PokemonModel> parseResponse(String response) throws JSONException {
         List<PokemonModel> pokemon = new ArrayList<>();
         JSONObject jsonObject = new JSONObject(response);
-        // TODO(nick) parse using JSONObject
+        JSONArray results = jsonObject.getJSONArray("results");
+        for (int i = 0; i < results.length(); ++i) {
+            JSONObject result = (JSONObject) results.get(i);
+            String name = result.getString("name");
+            String url = result.getString("url");
+            pokemon.add(new PokemonModel(name, url));
+        }
         return pokemon;
     }
 }
