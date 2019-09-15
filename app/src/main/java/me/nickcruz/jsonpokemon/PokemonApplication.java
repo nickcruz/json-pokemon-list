6194122e9157fbee5b7d3381ab42e97bcf522047
Nick Cruz <ncruz@umich.edu>
2019-09-15 12:27:56 -0700

    Add caching logic for Room DB.

--- app/src/main/java/me/nickcruz/jsonpokemon/PokemonApplication.java
+++ app/src/main/java/me/nickcruz/jsonpokemon/PokemonApplication.java
@@ -2,6 +2,10 @@ package me.nickcruz.jsonpokemon;
 
 import android.app.Application;
 
+import androidx.room.Room;
+
+import me.nickcruz.jsonpokemon.db.PokemonDatabase;
+
 public class PokemonApplication extends Application {
 
     private static PokemonApplication INSTANCE;
@@ -10,9 +14,16 @@ public class PokemonApplication extends Application {
         return INSTANCE;
     }
 
+    private PokemonDatabase pokemonDatabase;
+
     @Override
     public void onCreate() {
         super.onCreate();
         INSTANCE = this;
+        pokemonDatabase = Room.databaseBuilder(this, PokemonDatabase.class, "pokemon-db").build();
+    }
+
+    public PokemonDatabase getPokemonDatabase() {
+        return pokemonDatabase;
     }
 }
--- app/src/main/java/me/nickcruz/jsonpokemon/repository/PokemonRepository.java
+++ app/src/main/java/me/nickcruz/jsonpokemon/repository/PokemonRepository.java
@@ -1,44 +1,117 @@
 package me.nickcruz.jsonpokemon.repository;
 
-import android.app.Application;
+import android.text.format.DateUtils;
+import android.util.Log;
 
+import androidx.collection.ArrayMap;
+
+import com.android.volley.Request;
 import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import me.nickcruz.jsonpokemon.PokemonApplication;
 import me.nickcruz.jsonpokemon.PokemonModel;
 import me.nickcruz.jsonpokemon.api.GetPokemonEndpoint;
+import me.nickcruz.jsonpokemon.db.PokemonDatabase;
+import me.nickcruz.jsonpokemon.db.PokemonModelEntity;
 
 public class PokemonRepository implements GetPokemonEndpoint.GetPokemonResponseHandler {
 
-    private final Application application = PokemonApplication.getInstance();
+    private static final String TAG = PokemonRepository.class.getSimpleName();
+
+    private static final Long REQUEST_AGE_STALE = 10 * DateUtils.MINUTE_IN_MILLIS;
+
+    private final PokemonApplication application = PokemonApplication.getInstance();
+    private final PokemonDatabase pokemonDatabase = application.getPokemonDatabase();
     private final GetPokemonEndpoint getPokemonEndpoint = new GetPokemonEndpoint();
+    private final GetPokemonDatabaseWriter getPokemonDatabaseWriter = new GetPokemonDatabaseWriter(pokemonDatabase);
+
     private final List<GetPokemonEndpoint.GetPokemonResponseHandler> responseHandlers = new ArrayList<>();
+    private final Map<Request<?>, Long> requestTimestamps = new ArrayMap<>();
 
     public void addResponseHandler(GetPokemonEndpoint.GetPokemonResponseHandler responseHandler) {
         responseHandlers.add(responseHandler);
     }
 
     public void requestPokemon() {
-        // TODO(nick) If this was a recently-made call, skip the endpoint and call Room
-        // Otherwise, make the request, add a DB write response handler, then remove it
-        Volley.newRequestQueue(application)
-                .add(getPokemonEndpoint.getPokemon(this));
+        Request<String> request = getPokemonEndpoint.getPokemon(this);
+        Long requestAge = requestTimestamps.get(request);
+        long currentTimeMillis = System.currentTimeMillis();
+
+        // This will crash the app unless moved to a background thread
+        List<PokemonModelEntity> pokemonEntities = pokemonDatabase.pokemonDao().getAll();
+
+        if (pokemonEntities.isEmpty() || requestAge == null || currentTimeMillis - requestAge >= REQUEST_AGE_STALE) {
+            if (pokemonEntities.isEmpty()) {
+                Log.v(TAG, "No entities in db.");
+            } else if (requestAge == null) {
+                Log.v(TAG, "New request made: " + request.getUrl());
+            } else {
+                Log.v(TAG, String.format("Found matching stale request: %s, Timestamp: %s", request.getUrl(), requestAge));
+            }
+            requestPokemonFromNetwork(request, currentTimeMillis);
+        } else {
+            requestPokemonFromDatabase(pokemonEntities);
+        }
+    }
+
+    private void requestPokemonFromNetwork(Request<String> request, long currentTimeMillis) {
+        Log.v(TAG, String.format("Requesting pokemon from URL: %s", request.getUrl()));
+        responseHandlers.add(getPokemonDatabaseWriter);
+        Volley.newRequestQueue(application).add(request);
+        requestTimestamps.put(request, currentTimeMillis);
+    }
+
+    private void requestPokemonFromDatabase(List<PokemonModelEntity> entities) {
+        Log.v(TAG, "Requesting pokemon from db.");
+        List<PokemonModel> pokemon = new ArrayList<>(entities.size());
+        for (PokemonModelEntity entity : entities) {
+            pokemon.add(new PokemonModel(entity.name, entity.url));
+        }
+        onGetPokemonResponse(pokemon);
     }
 
     @Override
     public void onGetPokemonResponse(List<PokemonModel> pokemon) {
+        Log.v(TAG, String.format("onGetPokemonResponse: %s", pokemon.toString()));
         for (GetPokemonEndpoint.GetPokemonResponseHandler responseHandler : responseHandlers) {
             responseHandler.onGetPokemonResponse(pokemon);
         }
+        responseHandlers.remove(getPokemonDatabaseWriter);
     }
 
     @Override
     public void onError(String error) {
+        Log.w(TAG, String.format("onError: %s", error));
         for (GetPokemonEndpoint.GetPokemonResponseHandler responseHandler : responseHandlers) {
             responseHandler.onError(error);
+        }
+    }
+
+    private static class GetPokemonDatabaseWriter implements GetPokemonEndpoint.GetPokemonResponseHandler {
+
+        private PokemonDatabase pokemonDatabase;
+
+        public GetPokemonDatabaseWriter(PokemonDatabase pokemonDatabase) {
+            this.pokemonDatabase = pokemonDatabase;
+        }
+
+        @Override
+        public void onGetPokemonResponse(List<PokemonModel> pokemon) {
+            PokemonModelEntity[] entities = new PokemonModelEntity[pokemon.size()];
+            for (int i = 0; i < pokemon.size(); ++i) {
+                PokemonModel pokemonModel = pokemon.get(i);
+                entities[i] = new PokemonModelEntity(pokemonModel.getName(), pokemonModel.getUrl());
+            }
+            pokemonDatabase.pokemonDao().insertAll(entities);
+        }
+
+        @Override
+        public void onError(String error) {
+            // Do nothing
         }
     }
 }
